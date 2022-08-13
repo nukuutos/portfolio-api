34f9181eaabd17773bc35329ca29073e5e15805a
nukuutos <nukuutos@gmail.com>
2022-08-13 20:38:50 +1000

    Add condition to log in error handler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,9 +1,15 @@
+const { TEST } = require('../config/environments');
+
+const { NODE_ENV } = process.env;
+
 module.exports = (error, req, res, next) => {
   if (res.headersSent) return next(error);
 
   const { message, statusCode } = error;
 
-  console.log(message);
+  if (NODE_ENV !== TEST) {
+    console.log(message);
+  }
 
   return res.status(statusCode || 500).json({
     message: message || 'Server error occured. Please try again.',
