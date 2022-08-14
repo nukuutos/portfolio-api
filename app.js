83eec444e5ec8e3f8d86f52953ce8791c2554ac7
nukuutos <nukuutos@gmail.com>
2022-08-15 03:37:29 +1000

    Add cors

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const errorHandler = require('./middlewares/error-handler');
 const rateLimiter = require('./middlewares/rate-limiter');
 const validator = require('./middlewares/validator');
 const validate = require('./middlewares/validate');
+const cors = require('./middlewares/cors');
 
-app.use(express.json());
+app.use(cors);
 app.use(rateLimiter);
+app.use(express.json());
 
 app.use('/message', validator, validate, sendMessage);
 
